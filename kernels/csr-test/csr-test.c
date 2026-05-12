47b70020a8adcf3c541baea5761b9472a4bef6d0
srcres258 <src.res.211@gmail.com>
2026-05-12 08:37:18 +0800

    Fix CSR display
    Signed-off-by: srcres258 <src.res.211@gmail.com>

--- kernels/csr-test/csr-test.c
+++ kernels/csr-test/csr-test.c
@@ -1,14 +1,28 @@
 #include <stdint.h>
 #include <klib.h>
 
+static void put_hex(uint32_t val) {
+    static const char hex[] = "0123456789abcdef";
+    for (int i = 7; i >= 0; i--) {
+        putch(hex[(val >> (i * 4)) & 0xF]);
+    }
+}
+
+static void put_str(const char *s) {
+    for (; *s; s++) putch(*s);
+}
+
 int main() {
     uint32_t mvendorid, marchid;
 
     asm volatile("csrr %0, mvendorid" : "=r" (mvendorid));
     asm volatile("csrr %0, marchid" : "=r" (marchid));
 
-    printf("mvendorid: 0x%08x\n", mvendorid);
-    printf("marchid: %u\n", marchid);
+    put_str("mvendorid: 0x");
+    put_hex(mvendorid);
+    put_str("\nmarchid: 0x");
+    put_hex(marchid);
+    put_str(" (expected 0x017e8a6e = 25070190)\n");
 
     return 0;
 }
