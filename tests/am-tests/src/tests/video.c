e4d1a581d540f4fafe2c3f76b04092256621c0cb
srcres258 <src.res.211@gmail.com>
2026-05-19 22:02:43 +0800

    Add debug outputs
    Signed-off-by: srcres258 <src.res.211@gmail.com>

--- tests/am-tests/src/tests/video.c
+++ tests/am-tests/src/tests/video.c
@@ -15,6 +15,8 @@ static int used[N][N];
 
 static uint32_t color_buf[32 * 32];
 
+static inline void outl(uintptr_t addr, uint32_t data) { *(volatile uint32_t *)addr = data; }
+
 void redraw() {
   int w = io_read(AM_GPU_CONFIG).width / N;
   int h = io_read(AM_GPU_CONFIG).height / N;
@@ -22,7 +24,9 @@ void redraw() {
   assert((uint32_t)block_size <= LENGTH(color_buf));
 
   int x, y, k;
+  printf("Redrawing the canvas\n");
   for (y = 0; y < N; y ++) {
+    printf("y = %d\n", y);
     for (x = 0; x < N; x ++) {
       for (k = 0; k < block_size; k ++) {
         color_buf[k] = canvas[y][x];
@@ -44,16 +48,21 @@ void update() {
   static int dy[4] = {1, 0, -1, 0};
 
   tsc ++;
+  
+  printf("N=%d\n", N);
 
   for (int i = 0; i < N; i ++)
     for (int j = 0; j < N; j ++) {
       used[i][j] = 0;
     }
 
+  printf("Beginning to fill the canvas\n");
   int init = tsc * 1;
   canvas[0][0] = p(init); used[0][0] = 1;
   int x = 0, y = 0, d = 0;
   for (int step = 1; step < N * N; step ++) {
+    if (step % 256 == 0)
+      printf("Step %d\n", step);
     for (int t = 0; t < 4; t ++) {
       int x1 = x + dx[d], y1 = y + dy[d];
       if (x1 >= 0 && x1 < N && y1 >= 0 && y1 < N && !used[x1][y1]) {
@@ -65,16 +74,21 @@ void update() {
       d = (d + 1) % 4;
     }
   }
+  printf("Done filling the canvas\n");
 }
 
 void video_test() {
+  printf("Video device test\n");
+
   unsigned long last = 0;
   unsigned long fps_last = 0;
   int fps = 0;
 
   while (1) {
+    printf("Drawing\n");
     unsigned long upt = io_read(AM_TIMER_UPTIME).us / 1000;
     if (upt - last > 1000 / FPS) {
+      printf("Updating\n");
       update();
       redraw();
       last = upt;
@@ -86,5 +100,6 @@ void video_test() {
       fps_last = upt;
       fps = 0;
     }
+    printf("Loop done\n");
   }
 }
